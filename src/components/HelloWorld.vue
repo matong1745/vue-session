af6624af2bc1e3e782e9e9d783d62311e70f167b
Matong <matong1745@gmail.com>
2021-05-12 18:44:38 +0700

    Update version to 0.2.0

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    VERSION 0.2.0
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
